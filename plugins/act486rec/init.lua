d5f463e7e21bf2242ac67de675a32c01778d48ed
sairuk <sairuk@gmail.com>
2025-04-14 10:14:06 +1000

    add a stack to handle key combos

--- plugins/act486rec/init.lua
+++ plugins/act486rec/init.lua
@@ -13,13 +13,14 @@ local exports = {
 local act486auto = exports
 local reset_subscription
 local frame_subscription
-local frame = 0
-local lframe = 0
 
 function act486auto.startplugin()
 
-  local t
+  local t = {}
+  local frame = 0
+  local lframe = 0
   local rport = false
+  local dtokens = ""
 
   reset_subscription = emu.add_machine_reset_notifier(function()
     i = manager.machine.input
@@ -30,17 +31,37 @@ function act486auto.startplugin()
   -- for i,v in pairs(manager.machine.images) do print(i) end
   frame_subscription = emu.add_machine_frame_notifier(function()
     if manager.machine.time.seconds > 0 then
-      fstart = start
-      k = ik:poll()
-      action = false
-      if i:code_pressed(k) then
-        action = true
-        token = i:code_to_token(k)
+      p = ik:poll()
+
+      -- first time pressed, add to stack
+      if i:code_pressed(p) then
+        if t[p] == nil then
+          lframe = 0
+          dframe = 0
+          t[p] = { frame, lframe }
+        end
+
       end
     end
-    if action then
-      print('{ ' .. lframe .. ', 5, "' .. token .. '", "" },')
-      lframe = 0
+    if #t then
+      -- are any of these still pressed?
+      for k, v in pairs(t) do
+        if i:code_value(k) ~= 0 then
+          t[k] = { frame, lframe }
+          dframe = dframe + 1
+        else
+          t[k] = nil
+        end
+      end
+
+      tokens = ""
+      for k, v in pairs(t) do
+        tokens = tokens .. "," .. i:code_to_token(k)
+      end
+
+      if tokens ~= "" and lframe == 1 then
+        print('{ ' .. lframe .. ',' .. dframe .. '"' .. tokens .. '", "" },')
+      end
     end
     lframe = lframe + 1
     frame = frame + 1
